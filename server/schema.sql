bee2874dd1a1e680eb0c3ed5cf7ec2455861dc01
Shane Laymance <shane.laymance@gmail.com>
2017-09-18 20:51:10 -0700

    Initialize app and set up MySQL tables

--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,27 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE rooms (
+  id INT SIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  PRIMARY KEY(id)
+  );
+
+CREATE TABLE usernames (
+  id INT SIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  PRIMARY KEY(id)
+  );
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  message VARCHAR(255),
+  room INT SIGNED,
+  username INT SIGNED,
+  timeCreated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY(id),
+  FOREIGN KEY (room) REFERENCES rooms(id),
+  FOREIGN KEY (username) REFERENCES usernames(id)
 );
 
 /* Create other tables and define schemas for them here! */
