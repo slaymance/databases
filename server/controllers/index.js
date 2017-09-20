67135a9646063f28cf8799404da7cd69125e8e6b
Shane Laymance <shane.laymance@gmail.com>
2017-09-19 20:18:43 -0700

    Implement solution for post without checking usernames

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -9,7 +9,6 @@ module.exports = {
     }, // a function which handles a get request for all messages
     post: function (req, res) {
       models.messages.post(req.body, id => {
-        console.log(id);
         res.send({id: id});
       });
     } // a function which handles posting a message to the database
--- server/models/index.js
+++ server/models/index.js
@@ -10,7 +10,7 @@ module.exports = {
           }
           callback(results);
         });
-    }, // a function which produces all the messages
+    },
     post: function (message, callback) {
       db.query(`INSERT INTO messages (text, username) 
         SELECT '${message.text}', usernames.id
@@ -30,86 +30,3 @@ module.exports = {
     post: function () {}
   }
 };
-
-//       const textInsert = db.query(`INSERT INTO messages text VALUES '${message.text}`);
-//       const userQuery = db.query(`SELECT username FROM usernames WHERE username = '${message.username}`, []);
-
-//       textInsert.on('error', err => err);
-//       userQuery.on('error', err => err);
-//       userQuery.on('result', result => {
-//         if (!result.length) {
-//           const userInsert = db.query(`INSERT INTO usernames (username) VALUES '${message.username}'`);
-//           userInsert.on('error', err => err);
-//           userInsert.on('result', result => {
-//             const maxId = db.query(`SELECT MAX(id) FROM messages`, []);
-//             maxId.on('error', err => err);
-//             maxId.on('result', result => {
-//               const userId = db.query(`SELECT username.id FROM usernames 
-//                 INNER JOIN messages ON messages.id = ${values[0]} 
-//                 AND usernames.username = ${message.username}`, []);
-//               userId('error', err => err);
-//               userId('result', result => {
-
-//               })
-//             })
-//           })
-//         }
-//       });
-
-// const userId = db.query(`SELECT username.id FROM usernames 
-//               INNER JOIN messages ON messages.id = ${values[0]} 
-//               AND usernames.username = ${message.username}`, []);
-
-    //   let promiseArray = [];
-    //   const queryCallback = (err, results) => {
-    //     if (err) {
-    //       return err;
-    //     }
-    //     return results;
-    //   };
-    //   promiseArray.push(db.query(`SELECT username FROM usernames WHERE username = '${message.username}'`, [], queryCallback));
-    //   promiseArray.push(db.query(`INSERT INTO messages (text) VALUES ('${message.text}')`, (err, result) => {
-    //     if (err) {
-    //       return err;
-    //     }
-    //   }));
-    //   promiseArray.push(queryCallback);
-
-    //   Promise.all(promiseArray).then(values => {
-    //     let secondPromiseArray = [];
-    //     console.log(values);
-
-    //     secondPromiseArray.push(db.query('SELECT MAX(id) FROM messages', [], (err, result) => {
-    //       if (err) {
-    //         return err;
-    //       }
-    //       let postId = result[0]['MAX(id)'];
-    //       callback(postId);
-    //       return postId;
-    //     }));
-
-    //     if (!values[0].length) {
-    //       secondPromiseArray.push(db.query(`INSERT INTO usernames (username) VALUES ('${message.username}')`, (err, result) => {
-    //         if (err) {
-    //           return err;
-    //         }
-    //         console.log('SUCCESS');
-    //       }));
-    //     }
-
-    //     Promise.all(secondPromiseArray).then(values => {
-    //       db.query(`SELECT username.id FROM usernames 
-    //       INNER JOIN messages ON messages.id = ${values[0]} 
-    //       AND usernames.username = ${message.username}`, [], (err, result) => {
-    //         if (err) {
-    //           return err;
-    //         }
-    //         db.query(`INSERT INTO messages (username) VALUES ${result.id}`, (err, result) => {
-    //           if (err) {
-    //             return err;
-    //           }
-    //         });
-    //       });
-    //     });
-    //   });
-    // } // a function which can be used to insert a message into the database
